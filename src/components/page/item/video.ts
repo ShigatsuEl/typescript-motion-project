f98dcb5d59dcae0ac69974b3a3031caac2600bb8
MinChan Lee <shigatsu970704@gmail.com>
2021-02-15 10:16:53 +0900

    09.RegExp

--- src/components/page/item/video.ts
+++ src/components/page/item/video.ts
@@ -12,12 +12,21 @@ export class VideoComponent extends BaseComponent<HTMLVideoElement> {
     const iframeElement = this.element.querySelector(
       ".video__iframe"
     )! as HTMLIFrameElement;
-    iframeElement.src = "https://www.youtube.com/embed/K3-jG52XwuQ";
+    iframeElement.src = this.convertToEmbeddedURL(url);
 
     const titleElement = this.element.querySelector(
       ".video__title"
     )! as HTMLHeadingElement;
     titleElement.textContent = title;
-    console.log(url);
   }
+
+  private convertToEmbeddedURL = (url: string): string => {
+    const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+    const match = url.match(regExp);
+    const videoId = match ? match[1] || match[2] : undefined;
+    if (videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+    return url;
+  };
 }
