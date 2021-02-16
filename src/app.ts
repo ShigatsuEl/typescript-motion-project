5db6efc93534fc7e6249402a461a29e384a4807f
MinChan Lee <shigatsu970704@gmail.com>
2021-02-16 10:35:36 +0900

    13.Dialog Component(Create)

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import { VideoComponent } from "./components/page/item/video.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
 import { IComponent } from "./components/base.js";
+import { InputDialog } from "./components/dialog/dialog.js";
 
 class App {
   private readonly page: IComponent & IComposable;
@@ -32,6 +33,20 @@ class App {
 
     const todo = new TodoComponent("Todo Title", "Todo Item");
     this.page.addChild(todo);
+
+    const imageBtn = document.querySelector("#new-image")! as HTMLElement;
+    imageBtn.addEventListener("click", () => {
+      const dialog = new InputDialog();
+
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(document.body);
+      });
+      dialog.setOnSubmitListener(() => {
+        // 섹션을 만들어서 나중에 추가
+        dialog.removeFrom(document.body);
+      });
+      dialog.attachTo(document.body);
+    });
   }
 }
 
---
+++ src/components/dialog/dialog.ts
@@ -0,0 +1,42 @@
+import { BaseComponent, IComponent } from "../base.js";
+import { IComposable } from "../page/page.js";
+
+type OnCloseListener = () => void;
+type OnSubmitListener = () => void;
+
+export class InputDialog
+  extends BaseComponent<HTMLElement>
+  implements IComposable {
+  closeListener?: OnCloseListener;
+  submitListener?: OnSubmitListener;
+  constructor() {
+    super(`<dialog class="dialog">
+            <div class="dialog__container">
+              <button class="close">&times;</button>
+              <div id="dialog__body"></div>
+              <button class="dialog__submit">ADD</button>
+            </div>
+          </dialog>`);
+    const closeBtn = this.element.querySelector(".close")! as HTMLElement;
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener();
+    };
+    const submitBtn = this.element.querySelector(
+      ".dialog__submit"
+    )! as HTMLElement;
+    submitBtn.onclick = () => {
+      this.submitListener && this.submitListener();
+    };
+  }
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
+  }
+  setOnSubmitListener(listener: OnSubmitListener) {
+    this.submitListener = listener;
+  }
+
+  addChild(child: IComponent) {
+    const body = this.element.querySelector("#dialog__body")! as HTMLElement;
+    child.attachTo(body);
+  }
+}
